3e5d3739753449ed9f2910bcb544eec59dc73685
mandalapudisusanna <mandalapudisusanna@gmail.com>
2026-01-31 12:51:46 +0530

    Update Student.java

--- spring/src/com/javatpoint/Student.java
+++ spring/src/com/javatpoint/Student.java
@@ -1,22 +1,19 @@
 package com.javatpoint;
 
-public class Student {
+public class Student 
+{
+private String name;
 
-    private int id;
-    private String name;
-
-    // setter method
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    // setter method
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void displayInfo() {
-        System.out.println("Student Id: " + id);
-        System.out.println("Student Name: " + name);
-    }
+public String getName() {
+	return name;
 }
+
+public void setName(String name) {
+	this.name = name;
+}
+public void displayInfo()
+{
+System.out.println("Hello"+name);	
+}
+}
+
